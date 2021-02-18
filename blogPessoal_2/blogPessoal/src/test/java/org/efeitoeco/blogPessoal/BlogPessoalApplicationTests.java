23c88b08ab9004882a7bc19c8637ccf61ecda2d2
Gabriela Costa <gr.costa@outlook.com>
2021-02-18 11:22:33 -0300

    Blog com 'Delete&Post&Put'

---
+++ blogPessoal_2/blogPessoal/src/test/java/org/efeitoeco/blogPessoal/BlogPessoalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.efeitoeco.blogPessoal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogPessoalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
