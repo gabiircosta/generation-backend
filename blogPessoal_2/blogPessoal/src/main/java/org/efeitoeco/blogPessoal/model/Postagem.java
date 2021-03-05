684e29b826dc059335a75f116226a3c9834138c6
Gabriela Costa <gr.costa@outlook.com>
2021-03-05 11:41:04 -0300

    Swagger & Deploy

--- blogPessoal_2/blogPessoal/src/main/java/org/efeitoeco/blogPessoal/model/Postagem.java
+++ blogPessoal_2/blogPessoal/src/main/java/org/efeitoeco/blogPessoal/model/Postagem.java
@@ -12,10 +12,8 @@ import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-import org.springframework.lang.NonNull;
-
 @Entity
-@Table(name = "Postagem")
+@Table(name = "Postagens")
 public class Postagem {
 	
 	@Id
